4ddd4ee9ead4a117b7c7b3b72c0b72661777b4ec
Ranjeet Verma <ranjeetverma@Ranjeets-MacBook-Air.local>
2026-04-26 16:09:44 +0530

    new code

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,8 +26,8 @@ export default defineConfig({
   },
   projects: [
     { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+   // { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
+   // { name: 'webkit', use: { ...devices['Desktop Safari'] } },
   ],
   webServer: startLocalServer
     ? {
